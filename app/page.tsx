e7152f9499432305d40a3c0d1bcf68d73f843280
vitdolinek <dolinekvit@gmail.com>
2026-08-12 11:54:46 +0200

    Prettier, menu component and home page info

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,64 @@
-import Image from 'next/image'
+import { Box, Em, Heading, Image, Text, Link, DataList } from '@chakra-ui/react';
 
 export default function Home() {
-
   return (
-    <div className="flex flex-col align-center p-5">
-      <div className="mb-6">
-        ANIMATION
-      </div>
-      <Image
-        src="/profile.jpeg"
-        width={100}
-        height={250}
-        alt="Vít Dolínek"
-        className="w-28 h-28 flex m-auto bg-grey-600 rounded-full border-2"
-      />
-      <div>
-        <h1>Vít Dolínek</h1>
-        <span><i>Software engineer</i></span>
-      </div>
-  </div>
+    <Box display={'flex'} flexDir={'column'} gap={'6'}>
+      <div className="mb-6">ANIMATION</div>
+      <Box display={'flex'} gap={'3'} flexDir={'row'} justifyContent={'space-between'} alignItems={'center'}>
+        <Box display={'flex'} flexDir={'column'}>
+          <Heading>Vít Dolínek</Heading>
+          <span>
+            <Text>
+              <Em>Software engineer</Em>
+            </Text>
+          </span>
+        </Box>
+        <Box>
+          <Image src="/profile.jpeg" alt="Vít Dolínek" borderRadius={'full'} boxSize={'100px'} />
+        </Box>
+      </Box>
+      <Box>
+        <Heading>Work</Heading>
+        <Text>
+          I am currently working at Cisco company and am based in Olomouc, Czechia. My expertise is fullstack
+          development in React and NodeJS with more than 7 years of experience. I am big fan of CLI tools and make my
+          own dotfile configs that you can check out on my <Link href={'#'}>GitLab</Link>. I welcome challenges and try
+          to improve both my hard and soft skills.
+        </Text>
+      </Box>
+      <Box>
+        <Heading>Bio</Heading>
+        <DataList.Root gap={'1'} orientation={'horizontal'}>
+          <DataList.Item>
+            <DataList.ItemLabel minW={'50px'}>1998</DataList.ItemLabel>
+            <DataList.ItemValue>Born in Sternberk, Czechia</DataList.ItemValue>
+          </DataList.Item>
+          <DataList.Item>
+            <DataList.ItemLabel minW={'50px'}>2017</DataList.ItemLabel>
+            <DataList.ItemValue>High school diploma, SPS a SOU Unicov</DataList.ItemValue>
+          </DataList.Item>
+          <DataList.Item>
+            <DataList.ItemLabel minW={'50px'}>2017</DataList.ItemLabel>
+            <DataList.ItemValue>Web developer at Devmates</DataList.ItemValue>
+          </DataList.Item>
+          <DataList.Item>
+            <DataList.ItemLabel minW={'50px'}>2018</DataList.ItemLabel>
+            <DataList.ItemValue>Web developer at Orbinet</DataList.ItemValue>
+          </DataList.Item>
+          <DataList.Item>
+            <DataList.ItemLabel minW={'50px'}>2019</DataList.ItemLabel>
+            <DataList.ItemValue>Frontend developer at QCM</DataList.ItemValue>
+          </DataList.Item>
+          <DataList.Item>
+            <DataList.ItemLabel minW={'50px'}>2022</DataList.ItemLabel>
+            <DataList.ItemValue>Frontend developer at Smartlook</DataList.ItemValue>
+          </DataList.Item>
+          <DataList.Item>
+            <DataList.ItemLabel minW={'50px'}>2023</DataList.ItemLabel>
+            <DataList.ItemValue>Software engineer at Cisco</DataList.ItemValue>
+          </DataList.Item>
+        </DataList.Root>
+      </Box>
+    </Box>
   );
 }
--- components/menu.tsx
+++ components/menu.tsx
@@ -1,12 +1,25 @@
 import { Box } from '@chakra-ui/react';
-import NavLink from "./nav-link";
+import NavLink from './nav-link';
 
 export default function Menu() {
   return (
-    <Box borderBottom={'xs'} borderBottomColor={'border.subtle'} bg={'bg.subtle'} p={'3'} display={'flex'} justifyContent={'center'} gap={'3'} flexDir={'row'}>
-      <NavLink href={"/"} label={"Home"} />
-      <NavLink href={"/experience"} label={"Experience"} />
-      <NavLink href={"#"} label={"Source"} />
+    <Box
+      borderBottom={'xs'}
+      borderBottomColor={'border.subtle'}
+      bg={'bg.subtle'}
+      _light={{ bg: 'rgba(255, 255, 255, 0.5)', backdropFilter: 'blur(5px)' }}
+      _dark={{ bg: 'rgba(32, 32, 35, 0.5)', backdropFilter: 'blur(5px)' }}
+      p={'3'}
+      position={'sticky'}
+      top={'0'}
+      display={'flex'}
+      justifyContent={'center'}
+      gap={'3'}
+      flexDir={'row'}
+    >
+      <NavLink href={'/'} label={'Home'} />
+      <NavLink href={'/experience'} label={'Experience'} />
+      <NavLink href={'#'} label={'Source'} />
     </Box>
   );
 }
