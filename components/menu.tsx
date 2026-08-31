149993582c8a61d4fb9f78848bc4d4a1371e25aa
vitdolinek <dolinekvit@gmail.com>
2026-08-31 10:41:13 +0200

    add: initialize mobile menu

--- components/menu.tsx
+++ components/menu.tsx
@@ -1,27 +1,56 @@
+'use client';
 import { Box } from '@chakra-ui/react';
 import NavLink from './nav-link';
-import { GitHubLogoIcon } from "@radix-ui/react-icons";
+import { GitHubLogoIcon, HamburgerMenuIcon } from '@radix-ui/react-icons';
+import { useState } from 'react';
 
 export default function Menu() {
+  const [mobileMenuOpen, isMobileMenuOpen] = useState(false);
   return (
-    <Box
-      borderBottom={'xs'}
-      borderBottomColor={'border.subtle'}
-      bg={'bg.subtle'}
-      _light={{ bg: 'rgba(255, 255, 255, 0.5)', backdropFilter: 'blur(5px)' }}
-      _dark={{ bg: 'rgba(32, 32, 35, 0.5)', backdropFilter: 'blur(5px)' }}
-      p={'3'}
-      position={'sticky'}
-      top={'0'}
-      display={'flex'}
-      justifyContent={'center'}
-      gap={'3'}
-      flexDir={'row'}
-      zIndex={'2'}
-    >
-      <NavLink href={'/'} label={'Home'} />
-      <NavLink href={'/experience'} label={'Experience'} />
-      <NavLink href={'https://github.com/dolinekvit/portfolio'} label={'Source'} icon={<GitHubLogoIcon />} linkProps={{ target: '_blank' }}/>
-    </Box>
+    <>
+      <Box
+        _light={{ bg: 'rgba(255, 255, 255, 0.5)', backdropFilter: 'blur(5px)' }}
+        _dark={{ bg: 'rgba(32, 32, 35, 0.5)', backdropFilter: 'blur(5px)' }}
+        p={'3'}
+        position={'sticky'}
+        display={'flex'}
+        justifyContent={'center'}
+        top={'0'}
+        zIndex={'2'}
+      >
+        <Box display={{ base: 'none', sm: 'flex' }} gap={'3'}>
+          <NavLink href={'/'} label={'Home'} />
+          <NavLink href={'/experience'} label={'Experience'} />
+          <NavLink
+            href={'https://github.com/dolinekvit/portfolio'}
+            label={'Source'}
+            icon={<GitHubLogoIcon />}
+            linkProps={{ target: '_blank' }}
+          />
+        </Box>
+        <Box display={{ base: 'flex', sm: 'none' }}>
+          <HamburgerMenuIcon onClick={() => isMobileMenuOpen(!mobileMenuOpen)} />
+        </Box>
+      </Box>
+      {mobileMenuOpen && (
+        <Box
+          display={'flex'}
+          _light={{ bg: 'rgba(255, 255, 255, 0.5)', backdropFilter: 'blur(5px)' }}
+          _dark={{ bg: 'rgba(32, 32, 35, 0.5)', backdropFilter: 'blur(5px)' }}
+          flexDir={'column'}
+          alignItems={'center'}
+          gap={'1'}
+        >
+          <NavLink href={'/'} label={'Home'} />
+          <NavLink href={'/experience'} label={'Experience'} />
+          <NavLink
+            href={'https://github.com/dolinekvit/portfolio'}
+            label={'Source'}
+            icon={<GitHubLogoIcon />}
+            linkProps={{ target: '_blank' }}
+          />
+        </Box>
+      )}
+    </>
   );
 }
