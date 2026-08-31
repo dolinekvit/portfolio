dfc1891152a2aaf367dd8b29dea25dcfa8a17c95
vitdolinek <dolinekvit@gmail.com>
2026-08-31 11:31:13 +0200

    change: Home page layout

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,9 @@ import { PCModel } from '@/components/pc-model';
 export default function Home() {
   return (
     <Box display={'flex'} flexDir={'column'} gap={'6'}>
+      <Box display={'flex'} justifyContent={'center'}>
+        <PCModel />
+      </Box>
       <Box display={'flex'} gap={'3'} mt={'10'} flexDir={'column'} alignItems={'center'}>
         <Box>
           <Image src="/profile.jpeg" alt="Vít Dolínek" borderRadius={'full'} boxSize={'90px'} />
@@ -16,16 +19,14 @@ export default function Home() {
           </Text>
         </Box>
       </Box>
-      <Box display={'flex'} justifyContent={'center'}>
-        <PCModel />
-      </Box>
       <Box>
         <Heading>Work</Heading>
         <Text textStyle={'sm'}>
           I am currently working at Cisco company and am based in Olomouc, Czechia. My expertise is fullstack
           development in React and NodeJS with more than 7 years of experience. I am big fan of CLI tools and make my
-          own dotfile configs that you can check out on my <Link href={'https://github.com/dolinekvit/dotfiles'}>Github</Link>. I welcome challenges and try
-          to improve both my hard and soft skills.
+          own dotfile configs that you can check out on my{' '}
+          <Link href={'https://github.com/dolinekvit/dotfiles'}>Github</Link>. I welcome challenges and try to improve
+          both my hard and soft skills.
         </Text>
       </Box>
       <Box>
